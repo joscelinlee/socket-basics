41f60818484b928cc22772c487f954e5ebc00420
Joscelin <joscelinlee@gmail.com>
2015-12-14 13:45:03 +0800

    Add message to browser UI

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ io.on('connection', function(socket) { // individual connection/socket
 	socket.on('message', function(message) {
 		console.log('Message received: ' + message.text);
 
-		// io.emit // send to everybody including the person who sent it
-		socket.broadcast.emit('message', message); // send to everybody except the person who sent it. First atgument is the type and second argument contains the reference message parameter.
+		io.emit('message', message); // send to everybody including the person who sent it
+		//socket.broadcast.emit('message', message); // send to everybody except the person who sent it. First atgument is the type and second argument contains the reference message parameter.
 	});
 
 	socket.emit('message', {
